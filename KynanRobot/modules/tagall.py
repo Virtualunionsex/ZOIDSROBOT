eac150316af6f3124624bd498aec78260da82608
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-03-12 00:11:54 +0800

    Update tagall.py

--- KynanRobot/modules/tagall.py
+++ KynanRobot/modules/tagall.py
@@ -10,30 +10,36 @@ from KynanRobot import telethn as client
 spam_chats = []
 
 
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@all ?(.*)"))
+@client.on(events.NewMessage(pattern="^/tagall|@all|/all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
-        return await event.respond(
-            "__This command can be use in groups and channels!__"
-        )
+        return await event.respond("__This command can be use in groups and channels!__")
 
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
     except UserNotParticipantError:
         is_admin = False
     else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can mention all!__")
+        return await event.reply("__Only admins can mention all!__")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__Give me one argument!__")
+        return await event.reply("__Give me one argument!__")
     elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
@@ -42,30 +48,27 @@ async def mentionall(event):
         msg = await event.get_reply_message()
         if msg == None:
             return await event.respond(
-                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
-            )
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
     else:
-        return await event.respond(
-            "__Reply to a message or give me some text to mention others!__"
-        )
+        return await event.reply("__Reply to a message or give me some text to mention others!__")
 
     spam_chats.append(chat_id)
     usrnum = 0
-    usrtxt = ""
+    usrtxt = ''
     async for usr in client.iter_participants(chat_id):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"📱[{usr.first_name}](tg://user?id={usr.id})\n "
+        usrtxt += f"🤖[{usr.first_name}](tg://user?id={usr.id})\n "
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
-            await asyncio.sleep(3)
+            await asyncio.sleep(2)
             usrnum = 0
-            usrtxt = ""
+            usrtxt = ''
     try:
         spam_chats.remove(chat_id)
     except:
@@ -74,21 +77,29 @@ async def mentionall(event):
 
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
-    if not event.chat_id in spam_chats:
-        return await event.respond("__There is no proccess on going...__")
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
     except UserNotParticipantError:
         is_admin = False
     else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can execute this command!__")
-
+        return await event.reply("__Only admins can execute this command!__")
+    if not event.chat_id in spam_chats:
+        return await event.reply("__There is no proccess on going...__")
     else:
         try:
             spam_chats.remove(event.chat_id)
