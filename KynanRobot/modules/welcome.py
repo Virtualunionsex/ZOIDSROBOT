a1140bc63f1ffce2094c8bc3677ae14127f7d25c
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-05-07 01:35:56 +0800

    welcome.py

--- KynanRobot/modules/welcome.py
+++ KynanRobot/modules/welcome.py
@@ -40,14 +40,14 @@ from FallenRobot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from FallenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from FallenRobot.modules.helper_funcs.msg_types import get_welcome_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+from KynanRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from KynanRobot.modules.helper_funcs.msg_types import get_welcome_type
+from KynanRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.global_bans_sql import is_user_gbanned
+from KynanRobot.modules.log_channel import loggable
+from KynanRobot.modules.sql.global_bans_sql import is_user_gbanned
 
 VALID_WELCOME_FORMATTERS = [
     "first",
